b8024ffcd369d0b56c71e6c67e91d09da27793dc
bang <daiyuhang417@gmail.com>
2018-06-28 09:06:21 +0800

    add .gitignore file

--- springcloud/service-hi/src/main/java/com/aotian/servicehi/controller/ServiceHiController.java
+++ springcloud/service-hi/src/main/java/com/aotian/servicehi/controller/ServiceHiController.java
@@ -1,7 +1,6 @@
 package com.aotian.servicehi.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
