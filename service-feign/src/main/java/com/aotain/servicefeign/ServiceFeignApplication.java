e4eaedc427a256a416d69359020b61d0ce2ca8a4
bang <daiyuhang417@gmail.com>
2018-06-29 09:14:22 +0800

    增加hystrix dashboard支持

--- service-feign/src/main/java/com/aotain/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/aotain/servicefeign/ServiceFeignApplication.java
@@ -2,12 +2,18 @@ package com.aotain.servicefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
+// 启动断路器，如果要监控hystrix的流必须开启此注解，即使fegin已经通过属性
+@EnableCircuitBreaker
 public class ServiceFeignApplication {
 
 	public static void main(String[] args) {
